25bef1d9ab2a44b2adfcf0bc5f043013e4a80a54
Elena Petrenko <k-i-s-u-n-j-a@list.ru>
2019-09-29 11:30:11 +0500

    Правки стилей

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,9 +1,15 @@
 var intro = document.querySelector('.intro');
-var operators_block = intro.querySelector('.operators__list');
+
+if (intro) {
+  var operators_block = intro.querySelector('.operators__list');
+}
 
 var operatorsBlockClickHandler = function(evt) {
   var target = evt.target.closest('li');
   target ? target.classList.toggle('operators__item--active') : false;
 };
 
-operators_block.addEventListener('click', operatorsBlockClickHandler);
+if (operators_block) {
+  operators_block.addEventListener('click', operatorsBlockClickHandler);
+}
+
